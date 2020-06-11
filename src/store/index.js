4d4938a5d4af46c6acb748862ada9e1f5e221224
Matsumoto Miu <miu.matsumoto04@gmail.comgit config --global user.email miu.matsumoto04@gmail.com>
2020-06-11 10:42:19 +0900

    feat:learn getters, mutation

--- src/store/index.js
+++ src/store/index.js
@@ -7,12 +7,33 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     todos: [
-      new Todo('タスク1'),
+      new Todo('タスク1', true),
       new Todo('タスク2'),
-      new Todo('タスク3'),
+      new Todo('タスク3', true),
     ]
   },
-  mutations: {},
+  getters: {
+    completedTodos(state) {
+      return state.todos.filter(todo => todo.completed);
+    },
+    completedTodosCount(state, getters) {
+      return getters.completedTodos.length;
+    }
+  },
+  mutations: {
+    completeTodo(state, payload) {
+      const todo = state.todos.find(todo => todo.id === payload.id);
+      if (todo) {
+        todo.finish();
+      }
+    },
+    initTodo(state, payload) {
+      const todo = state.todos.find(todo => todo.id === payload.id);
+      if (todo) {
+        todo.init();
+      }
+    },
+  },
   actions: {},
   modules: {}
 })
